3fe75c88d3cb8d39acaeb1d8258944faf16b7b2f
hyuk <hyuk.wsu@gmail.com>
2025-09-10 17:52:44 +0900

    add main menu_1

--- main_game.py
+++ main_game.py
@@ -1,14 +1,41 @@
-#메뉴 화면 만들기
-import pygame
+from main_menu import draw_main_menu
+import pygame, sys
        
-#기본 메뉴 설정
+#pygame 기본 초기화
 pygame.init()
-Screen = pygame.display.set_mode((800,600))
+
+#pygame 기본 옵션 설정
+Screen_width_size = 800
+Screen_height_size = 600
+Screen = pygame.display.set_mode((Screen_width_size, Screen_height_size))
+title_name = "Pac-Man game"
+pygame.display.set_caption(title_name)
+game_state = "Start Menu"
+#pygame 내의 필요한 설정
+
 clock = pygame.time.Clock()
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+#메인 함수
 running = True
-title = "Pac-Man"
 
 while running:
-    pygame.display.flip()
+    #FPS 설정
     clock.tick(60)
-pygame.quit()
+    # 각종 입력 감지
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        if game_state == "Start Menu":
+            darw_main_menu.menu()
+            
+
+    
+    #화면 그리기
+    Screen.fill(black)
+
+
+    #업데이트
+    pygame.display.flip()
---
+++ main_menu.py
@@ -0,0 +1,15 @@
+from main_game import *
+import pygame, sys
+
+pygame.init()
+class draw_main_menu:
+  def menu():
+    while True:
+      Screen.fill(black)
+      font = pygame.font.SysFont('arial', 80)
+      title = font.render('Pac-Man Game', True, black)
+      start_button = font.render('Start', True, black)
+      Screen.blit(title, (Screen_width_size/2)-(title.get_width()/2), (Screen_height_size/2)-((title.get_height)/2))
+      Screen.blit(start_button, (Screen_width_size/2)-(title.get_width()/2), (Screen_height_size/2)+((title.get_height)/2))
+      pygame.display.update()
+      
